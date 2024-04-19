956a673ee8b07a223fb8ec116432576138a749b1
Fabri Cost <you@example.com>
2024-04-19 09:32:07 -0300

    Trabajo terminado, se hicieron los manejos de casos especiales

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 def analisis_estadistico(valores):
-    
+    # Validar que la lista de valores no esté vacía
+    if not valores:
+        return ("La lista de valores está vacía")
+    # Validar que la lista de valores tenga solo números
+    if not all(isinstance(x, (int, float)) for x in valores):
+        return ("La lista de valores contiene elementos no numéricos.")
+    # Validar que el argumento sea una lista
+    if not isinstance(valores, list):
+        return ("El argumento no es una lista.")
     # Crear un DataFrame a partir de la lista de valores
     df = pd.DataFrame(valores, columns=["fi"])
     
